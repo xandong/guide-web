863b4d2ab4f5001b3b48d7c3a38fc6321ccf8ed0
Alexandre Gurgel <alexandre.gurgel@problemcompany.org>
2023-05-04 21:48:09 -0300

    feat: implements CardSingle

--- src/components/home/CardSingle.tsx
+++ src/components/home/CardSingle.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import { CardSection } from "../../utils/model/CardSection";
+import { Link } from "react-router-dom";
 
-const CardSingle: React.FC<CardSection> = (props: CardSection) => {
-  const { label } = props;
+interface CardSingleProps {
+  option: CardSection;
+}
+
+const CardSingle: React.FC<CardSingleProps> = (props: CardSingleProps) => {
+  const { option } = props;
   return (
     <React.Fragment>
-      <label htmlFor="">{label}</label>
+      <Link
+        to="/"
+        className="relative w-fit h-fit rounded-lg overflow-hidden shadow-md hover:shadow-xl shadow-zinc-200 hover:shadow-zinc-300 transition-all duration-300 hover:scale-[1.025] ease-in-out group"
+      >
+        <img
+          id={option.label}
+          className="sm:w-60 shadow-inner"
+          src={option.photo}
+          alt={option.label}
+        />
+        <label
+          htmlFor={option.label}
+          className="font-bold text-2xl text-white bg-black/10 px-2 py-px rounded-full absolute bottom-3 right-3 group-hover:text-[1.7rem] shadow-xl transition-all duration-500"
+        >
+          {option.label}
+        </label>
+      </Link>
     </React.Fragment>
   );
 };
