c9369509f77fea813bbd095cc48d0d17df2f11bf
Alexandre Gurgel <alexandre.gurgel@problemcompany.org>
2023-05-04 21:50:56 -0300

    feat: add options in Home

--- src/components/home/CardsList.tsx
+++ src/components/home/CardsList.tsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { CardSection } from "../../utils/model/CardSection";
 import CardSingle from "./CardSingle";
+import eventPic from "../../assets/event.png";
+import tourismPic from "../../assets/tourism.png";
+import servicePic from "../../assets/service.png";
 
 const options: CardSection[] = [
- 
+  { label: "Eventos", photo: eventPic, path: "/events" },
+  { label: "Turismo", photo: tourismPic, path: "/tourism" },
+  { label: "Serviços", photo: servicePic, path: "/services" },
 ];
 // Filter: Eventos, Turismo, Serviços
 
 const CardsList: React.FC = () => {
   return (
     <React.Fragment>
-      <div className="w-full flex justify-start">
-      <h2 className="text-3xl font-bold text-blue-500">Recomendados</h2>
+      <div className="w-full flex justify-start mb-6">
+        <h2 className="text-3xl font-bold text-blue-500">Recomendados</h2>
       </div>
-      <section className="grid grid-cols-3 justify-center justify-items-start space-x-8">
+      <section className="h-full grid grid-cols-3 justify-center items-center space-x-8 mb-6">
         {options.map((option: CardSection) => (
-          <CardSingle label={option.label} photo={option.photo} />
+          <CardSingle key={option.label} option={option} />
         ))}
       </section>
     </React.Fragment>
